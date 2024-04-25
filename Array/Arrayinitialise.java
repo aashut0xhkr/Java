3c4d4a5b7d4082e2a0fa2e6e3821e600138974af
Ashutosh K <167697535+aashut0xhkr@users.noreply.github.com>
2024-04-25 16:17:43 +0530

    Add files via upload

---
+++ Array/Arrayinitialise.java
@@ -0,0 +1,9 @@
+package Array;
+
+public class Arrayinitialise {
+    public static void main(String[] args) {
+        int[] arr= new int[5];
+        arr[0]=1;
+        System.out.println(arr[0]);
+    }
+}
