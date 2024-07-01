6f6fb8578d92697446332a6144458cb880c4a014
Ashutosh K <167697535+aashut0xhkr@users.noreply.github.com>
2024-07-01 12:13:41 +0530

    Add files via upload

---
+++ Square/square4.java
@@ -0,0 +1,10 @@
+public class square4{
+    public static void main(String[] args){
+        for (int i = 1; i <=4; i++) {
+            for(int j=1;j<=4;j++){
+              System.out.print((char)(j+64));
+            }
+            System.out.println();
+        }
+    }
+}
