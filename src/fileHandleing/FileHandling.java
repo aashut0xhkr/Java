2657b448bdc941a6f15a8768e7ae53fcc198613b
Ashutosh <aashut0xh@gmail.com>
2025-05-24 12:06:56 +0530

    update 1.0

---
+++ src/fileHandleing/FileHandling.java
@@ -0,0 +1,11 @@
+package fileHandleing;
+
+import java.io.File;
+import java.io.IOException;
+
+public class FileHandling {
+    public static void main(String[] args) throws IOException {
+        File f=new File("E:\\PSA\\src\\fileHandleing\\ashu.txt");
+        f.createNewFile();
+    }
+}
